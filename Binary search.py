d400a280f09dbfcd94d02ff14fbfa85d9e171c6b
ABHINAV <naliqbhi@gmail.com>
2026-01-04 21:43:00 +0530

    Binarysearch

---
+++ Binary search.py
@@ -0,0 +1,20 @@
+arr = [10, 20, 30, 40, 50]
+key = int(input("Enter element to search: "))
+
+low, high = 0, len(arr) - 1
+found = False
+
+while low <= high:
+    mid = (low + high) // 2
+    if arr[mid] == key:
+        found = True
+        break
+    elif key < arr[mid]:
+        high = mid - 1
+    else:
+        low = mid + 1
+
+if found:
+    print("Element found")
+else:
+    print("Element not found")
